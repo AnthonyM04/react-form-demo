939063ac02dd7d798e446a49ef180701d9c0ff2d
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-18 11:51:18 -0400

    timeout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  console.log(name)
+  const updateInput = (e) => {
+    const newName = e.target.value
+    // setTimeout(() => {
+      setName(newName)
+    // }, 20)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <form>
           <label htmlFor="name">
-            Name:
-            <input type="text" name="Plop"/>
+            Name: 
+            <input type="text" name="name"
+              onChange={updateInput} value={name}/>
           </label>
         </form>
       </header>
